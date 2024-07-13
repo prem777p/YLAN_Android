79c16aa7e491940d0a8b11f7b6eb68a13722ec40
Prem Sharma <prem767p@gmail.com>
2024-07-13 11:58:52 +0530

    add upload file feature

--- app/src/main/java/com/prem/ylan/api/RetrofitService.kt
+++ app/src/main/java/com/prem/ylan/api/RetrofitService.kt
@@ -1,14 +1,18 @@
 package com.prem.ylan.api
 
 
-import androidx.annotation.RawRes
 import com.prem.ylan.model.DirectoryFileInfo
 import com.prem.ylan.model.OsInfo
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.Part
 
 interface RetrofitService {
         @POST("/receivepath")
@@ -16,5 +20,10 @@ interface RetrofitService {
 
         @GET("/osinfo")
         fun getOsInfo(): Call<OsInfo?>?
+
+        @Multipart
+        @POST("/upload")
+        suspend fun uploadFile(@Part file: MultipartBody.Part, @Part("path") path: RequestBody): Response<String>
+
 }
 
