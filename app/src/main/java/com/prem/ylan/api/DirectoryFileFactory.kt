248bb44ddef9db109b1f3b07e98a7fa904ca38ed
Prem Sharma <prem767p@gmail.com>
2024-07-13 12:02:13 +0530

    add uploadRepository in function

--- app/src/main/java/com/prem/ylan/api/DirectoryFileFactory.kt
+++ app/src/main/java/com/prem/ylan/api/DirectoryFileFactory.kt
@@ -3,10 +3,11 @@ package com.prem.ylan.api
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.prem.ylan.api.repository.DirectoryFileRepository
-import com.prem.ylan.viewmodel.DirectoryFileViewModel
+import com.prem.ylan.api.repository.UploadRepository
+import com.prem.ylan.viewmodel.FileManagerViewModel
 
-class DirectoryFileFactory(private val repository: DirectoryFileRepository,val path : String) : ViewModelProvider.Factory {
+class DirectoryFileFactory(private val repository: DirectoryFileRepository, val path : String, private val uploadRepository: UploadRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return DirectoryFileViewModel(repository, path) as T
+        return FileManagerViewModel(repository, path, uploadRepository) as T
     }
 }
